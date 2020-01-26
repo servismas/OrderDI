0e1e811d5a3d72f93257183fc1e3cd748264a5c7
servismas <servismas@gmail.com>
2020-01-26 18:20:33 +0200

    Added DI property

--- src/OrderService.java
+++ src/OrderService.java
@@ -2,13 +2,21 @@
 public class OrderService {
 
 	private EmailService emailService;
-	
-	public OrderService(EmailService emailService) {
-		this.emailService = emailService;
-	}
+//	public OrderService(EmailService emailService) {
+//		
+//		this.emailService = emailService;
+//	}
 	
 	public void makeOrder() {
 		emailService.sendMail();
 		System.out.println("Ordered");
+	}
+
+	public EmailService getEmailService() {
+		return emailService;
+	}
+
+	public void setEmailService(EmailService emailService) {
+		this.emailService = emailService;
 	}
 }
